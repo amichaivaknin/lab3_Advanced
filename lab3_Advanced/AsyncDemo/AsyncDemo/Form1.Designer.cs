1c5100cfe331e65cd4b6dd81f0fe39f2c3b755c9
AmichaiVak <amiko565@gmail.com>
2016-08-16 23:16:25 +0300

    Add files via upload

---
+++ lab3_Advanced/AsyncDemo/AsyncDemo/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace AsyncDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.primesList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(25, 34);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(95, 20);
+            this.num1.TabIndex = 0;
+            this.num1.Text = "Lower limit";
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(157, 34);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(100, 20);
+            this.num2.TabIndex = 1;
+            this.num2.Text = "Upper limit";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(302, 34);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(123, 23);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "calculate primes";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // primesList
+            // 
+            this.primesList.FormattingEnabled = true;
+            this.primesList.Location = new System.Drawing.Point(108, 84);
+            this.primesList.Name = "primesList";
+            this.primesList.Size = new System.Drawing.Size(177, 134);
+            this.primesList.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 261);
+            this.Controls.Add(this.primesList);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.ListBox primesList;
+    }
+}
+
---
+++ lab3_Advanced/AsyncDemo/AsyncDemo/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncDemo
+{
+    public partial class Form1 : Form
+    {
+        internal delegate IEnumerable<int> PrimesDelegate(int num1, int num2);
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            PrimesDelegate primesDelegate = CalcPrimes;
+            primesList.Items.Clear();
+            int number1;
+            int number2;
+            if (!int.TryParse(num1.Text,out number1))
+            {
+                primesList.Items.Add("Wrong number1, int only");
+                return;
+            }
+
+            if (!int.TryParse(num2.Text, out number2))
+            {
+                primesList.Items.Add("Wrong number2, int only");
+                return;
+            }
+
+            primesDelegate.BeginInvoke(number1, number2,ar=>CallBack(primesDelegate,ar), primesDelegate);
+        }
+
+        private void CallBack(PrimesDelegate pd, IAsyncResult ar)
+        {
+            var list = pd.EndInvoke(ar);
+            primesList.Invoke(new Action(() =>
+            {
+                foreach (var prime in list)
+                {
+                    primesList.Items.Add(prime);
+                }
+            }));
+        }
+
+        private IEnumerable<int> CalcPrimes(int number1, int number2)
+        {
+            var primeList = new List<int>();
+
+            if (number2<number1||number1<0)
+            {
+                return primeList;
+            }
+
+            for (var i = number1; i <= number2; i++)
+            {
+                if (IsPrime(i))
+                {
+                    primeList.Add(i);
+                }
+            }
+            return primeList;
+        }
+
+        private static bool IsPrime(int num)
+        {
+            if (num<2)
+            {
+                return false;
+            }
+            if (num==2||num==3)
+            {
+                return true;
+            }
+
+            for (var i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num%i==0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
